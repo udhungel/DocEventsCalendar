cf47926d923700a068b5c1c6fabb9fc6c8c28796
udhungel <udeepdhungel@gmail.com>
2025-03-13 20:47:57 +0100

    Request & Response Dto for Attendees

---
+++ DTOs/RequestAttendeeDto.cs
@@ -0,0 +1,8 @@
+﻿namespace DocEventsAttendeeCalendar.DTOs
+{
+    public class RequestAttendeeDto
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
