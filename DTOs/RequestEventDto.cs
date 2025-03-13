724f7b4e3ef243f792dc066086ab36c2d5e2d463
udhungel <udeepdhungel@gmail.com>
2025-03-13 22:06:35 +0100

    Removed Id from RequestEventDto

--- DTOs/RequestEventDto.cs
+++ DTOs/RequestEventDto.cs
@@ -1,8 +1,7 @@
 ﻿namespace DocEventsAttendeeCalendar.DTOs
 {
     public class RequestEventDto
-    {
-        public int Id { get; set; }
+    {       
         public string Title { get; set; }
         public string Description { get; set; }
         public DateTime StartTime { get; set; }
