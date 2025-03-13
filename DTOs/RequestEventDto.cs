2a7e1444c8fcc3a79131aadb7f1b7cf676f73d18
udhungel <udeepdhungel@gmail.com>
2025-03-13 20:42:31 +0100

    Added Request Response Dto for the Events

---
+++ DTOs/RequestEventDto.cs
@@ -0,0 +1,10 @@
+﻿namespace DocEventsAttendeeCalendar.DTOs
+{
+    public class RequestEventDto
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+    }
+}
---
+++ DTOs/ResponseEventDto.cs
@@ -0,0 +1,11 @@
+﻿namespace DocEventsAttendeeCalendar.DTOs
+{
+    public class ResponseEventDto
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+    }
+}
+
